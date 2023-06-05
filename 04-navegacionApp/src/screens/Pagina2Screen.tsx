2c7b1cc3077e28eaa0678432bffb9d05bf3e2dca
Feliciano07 <fernandoxhajon122@gmail.com>
2023-06-05 12:00:23 -0600

    Inicio proyecto navegacion

---
+++ 04-navegacionApp/src/screens/Pagina2Screen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Pagina2Screen = () => {
+
+    return(
+        <View>
+            <Text>
+                Pagina 2
+            </Text>
+        </View>
+    )
+}
+
+export default Pagina2Screen;
